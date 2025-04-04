e5b7a9172af89fb90547e5b1480480dc55ff41bf
Marte Fandrem <70686197+martef@users.noreply.github.com>
2025-04-04 17:25:02 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages(c("tidyverse", "vegan", "lme4", "quarto", "ggrepel", "ggridges", "RColorBrewer", "scales", "gridExtra", "brms", "posterior", "rstan", "bayesplot", "loo", "sjPlot", "grid"))
+install.packages(c("tidyverse", "vegan", "lme4", "quarto", "ggrepel", "ggridges", "RColorBrewer", "scales", "gridExtra", "brms", "posterior", "rstan", "bayesplot", "loo", "sjPlot", "grid", "virides", "knitr"))
